c8ef7acb0715ef8b3967e3a233e90a7193a9e5ad
JoseGCheca <Jose.GCheca@gmail.com>
2015-12-31 16:17:15 +0100

    added angular material client

--- server/noiseMaps/app.js
+++ server/noiseMaps/app.js
@@ -10,11 +10,12 @@ var sensors = require('./routes/sensorsRoutes');
 var app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join('../../web-client/', 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -55,5 +56,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.get('*', function(req, res) {
+    res.redirect('/#' + req.originalUrl);
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
 
 module.exports = app;
